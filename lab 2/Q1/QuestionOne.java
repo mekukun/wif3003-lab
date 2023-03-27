dc984129d282d9b96ad4f7d36cd0827d4fb10c76
meku <piangree147@gmail.com>
2023-03-28 01:54:46 +0800

    Lab 1 + Lab 2 Finished

---
+++ lab 2/Q1/QuestionOne.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+
+public class QuestionOne {
+    public static void main(String[] args) {
+        int[] numbers = new int[1000000];
+        Random r = new Random();
+        int max = 0;
+
+        // fill the array with random numbers
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = r.nextInt(50000) + 1;
+        }
+
+        Timer t = new Timer();
+
+        // find the largest number
+        t.start();
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        t.stop();
+
+        System.out.println("The largest number is: " + max);
+        System.out.println("Performance (ns): " + t.getDuration());
+    }
+
+    private static class Timer {
+        private long startTime;
+        private long endTime;
+
+        public void start() {
+            startTime = System.nanoTime();
+        }
+
+        public void stop() {
+            endTime = System.nanoTime();
+        }
+
+        public long getDuration() {
+            return (endTime - startTime);
+        }
+    }
+}
---
+++ lab 2/Q2/QuestionTwo.java
@@ -0,0 +1,66 @@
+package Q2;
+
+import java.util.Random;
+
+public class QuestionTwo {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[1000000];
+        Random r = new Random();
+
+        // fill the array with random numbers
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = r.nextInt(50000) + 1;
+        }
+
+        Thread t1 = new Thread(new FindMaxRunnable(numbers));
+        Thread t2 = new Thread(new FindMaxRunnable(numbers));
+        t1.start();
+        t2.start();
+    }
+
+    private static class FindMaxRunnable implements Runnable {
+
+        private int[] numbers;
+
+        public FindMaxRunnable(int[] numbers) {
+            this.numbers = numbers;
+        }
+
+        public void run() {
+
+            Timer t = new Timer();
+
+            int max = 0;
+
+            t.start();
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] > max) {
+                    max = numbers[i];
+                }
+            }
+            t.stop();
+
+            System.out.println(Thread.currentThread().getName() + ": " + max);
+            System.out.println(Thread.currentThread().getName() + "'s performance: " + t.getDuration());
+        }
+    }
+
+    private static class Timer {
+        private long startTime;
+        private long endTime;
+
+        public void start() {
+            startTime = System.nanoTime();
+        }
+
+        public void stop() {
+            endTime = System.nanoTime();
+        }
+
+        public long getDuration() {
+            return (endTime - startTime);
+        }
+    }
+
+}
